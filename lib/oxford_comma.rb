cc0b6bc35c35eae9fc362ed781ad7abc11a1430e
Joseph Vint <josephvint@me.com>
2018-02-11 17:04:47 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
     array.join
   elsif array.size == 2
     array.join(" and ")
-  elsif array.size == 3
+  elsif array.size >= 3
     array[-1] = "and " + array[-1]
     array.join(", ")
   end
